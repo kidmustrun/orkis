05484ebfc234b5a6c17fde30022488136a0fbce9
Irina <parakerta@mail.ru>
2021-11-17 22:40:52 +0300

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,26 +1,25 @@
 const axios = require("axios").default;
-const URL_BASE = "http://7837-95-165-9-250.ngrok.io";
+const URL_BASE = "http://57ef-95-165-9-250.ngrok.io";
 const requestHeaders = {
   "Access-Control-Allow-Origin": "*",
-  'Accept': "application/json",
+  Accept: "application/json",
   "Content-Type": "application/json",
   "X-Requested-With": "XMLHttpRequest",
 };
-export const loginUser = user => { console.log(user);
-    axios
+export const loginUser = (user) => {
+  console.log(user);
+  axios
     .post(
       `${URL_BASE}/login`,
-      {
-        user
-      },
+      user,
       requestHeaders
     )
     .then((response) => {
-      localStorage.setItem('token', response.headers.authorization);
-      console.log(localStorage.getItem('token'))
+      localStorage.setItem("token", response.headers.authorization);
+      console.log(localStorage.getItem("token"));
     })
     .catch(function (error) {
-      localStorage.removeItem('token');
+      localStorage.removeItem("token");
       console.error("Something bad happened", error);
     });
-}
+};
